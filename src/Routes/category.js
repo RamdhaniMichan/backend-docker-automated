1176f4043d88b46397c2ac1c4eefc9a87cc74d6f
Michan11 <ramdhani.michan48@gmail.com>
2021-02-05 09:27:27 +0700

    edit routes category

--- src/Routes/category.js
+++ src/Routes/category.js
@@ -4,9 +4,9 @@ const routes = express.Router();
 const controller = require("../Controllers/category");
 const validate = require("../Middleware/validate");
 
-routes.get("/", validate("admin"), controller.get);
-routes.post("/", validate("admin"), controller.add);
-routes.put("/", validate("admin"), controller.update);
-routes.delete("/:id", validate("admin"), controller.del);
+routes.get("/", validate(["admin"]), controller.get);
+routes.post("/", validate(["admin"]), controller.add);
+routes.put("/", validate(["admin"]), controller.update);
+routes.delete("/:id", validate(["admin"]), controller.del);
 
 module.exports = routes;
